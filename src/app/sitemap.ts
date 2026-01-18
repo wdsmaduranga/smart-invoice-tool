cf3b6cdf6d20635e2b823c6d0586090a52e7a056
Sanka Maduranga <54570418+wdsmaduranga@users.noreply.github.com>
2026-01-18 21:04:30 +0530

    new update version 2 3

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -11,35 +11,35 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'weekly',
       priority: 1.0,
     },
-    // {
-    //   url: `${baseUrl}/about`,
-    //   lastModified: currentDate,
-    //   changeFrequency: 'monthly',
-    //   priority: 0.8,
-    // },
-    // {
-    //   url: `${baseUrl}/contact`,
-    //   lastModified: currentDate,
-    //   changeFrequency: 'monthly',
-    //   priority: 0.7,
-    // },
-    // {
-    //   url: `${baseUrl}/privacy`,
-    //   lastModified: currentDate,
-    //   changeFrequency: 'yearly',
-    //   priority: 0.5,
-    // },
-    // {
-    //   url: `${baseUrl}/terms`,
-    //   lastModified: currentDate,
-    //   changeFrequency: 'yearly',
-    //   priority: 0.5,
-    // },
-    // {
-    //   url: `${baseUrl}/faq`,
-    //   lastModified: currentDate,
-    //   changeFrequency: 'monthly',
-    //   priority: 0.6,
-    // },
+    {
+      url: `${baseUrl}/about`,
+      lastModified: currentDate,
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/contact`,
+      lastModified: currentDate,
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+    {
+      url: `${baseUrl}/privacy`,
+      lastModified: currentDate,
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
+    {
+      url: `${baseUrl}/terms`,
+      lastModified: currentDate,
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
+    {
+      url: `${baseUrl}/faq`,
+      lastModified: currentDate,
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
   ]
 }
